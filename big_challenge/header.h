b3ba482d3d11b25787f5dee320f41818dd3a9c37
Iza-Hi <iza.25@mhs.usk.ac.id>
2025-11-25 13:54:06 +0700

    struct

--- big_challenge/header.h
+++ big_challenge/header.h
@@ -1,3 +1,15 @@
 void olah_teks();
 void simpan_ke_biner();
 void ambil_dari_biner(int);
+
+typedef struct {
+    int panjang_kata;
+    char *kata;
+    int frekuensi;
+}Kata;
+
+typedef struct {
+    char abjad;
+    int jumlah_kata;
+    Kata *daftar_kata;
+}Abjad;
