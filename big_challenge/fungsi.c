9767e05d72c17a1c2f043133a420bfe9da3583c5
dzakybelajar <dzakysyraj@mhs.usk.ac.id>
2025-12-01 20:53:06 +0700

    tes

--- big_challenge/fungsi.c
+++ big_challenge/fungsi.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
 
 typedef struct {
     int panjang_kata;
@@ -12,6 +13,9 @@ typedef struct {
     int jumlah_kata;
     Kata *daftar_kata;
 }Abjad;
+
+Abjad abjad[26];
+Abjad abjd_baca[26];
 
 void olah_teks(char file[]){
     char buffer[500000];
@@ -51,12 +55,10 @@ void olah_teks(char file[]){
     fclose(file);
 }
 
-void pengurutan(Abjad abjad[]){
-
+void pengurutan(){
     //urutin frekuensi
     for (int i = 0; i < 26; i++)
-    {   if (abjad[i].jumlah_kata==1)
-            { continue; }
+    {   
         for ( int n=0; n < abjad[i].jumlah_kata-1; n++)
         {
             for (int j=n+1; j > 0; j--)
@@ -72,9 +74,8 @@ void pengurutan(Abjad abjad[]){
     } 
 
     //urutin berdasarkan panjang kata atau besar kecilnya kata jika frekuensinya sama
-     for (int i = 0; i < 26; i++)
-    {   if (abjad[i].jumlah_kata==1)
-            { continue; }
+    for (int i = 0; i < 26; i++)
+    {   
         for ( int n=0; n < abjad[i].jumlah_kata-1; n++)
         {
             for (int j=n+1; j > 0; j--)
@@ -103,19 +104,19 @@ void pengurutan(Abjad abjad[]){
     } 
 }
 
-void simpan_ke_biner(Abjad abjd[]){
+void simpan_ke_biner(){
    FILE *fp=fopen("binary.bin","wb");
    int n=0;
     while (n!=26)
     {   
-        fwrite(&abjd[n].abjad,sizeof(char),1,fp);
-        fwrite(&abjd[n].jumlah_kata,sizeof(int),1,fp);
+        fwrite(&abjad[n].abjad,sizeof(char),1,fp);
+        fwrite(&abjad[n].jumlah_kata,sizeof(int),1,fp);
         int i=0;
-        while (i!=abjd[n].jumlah_kata)
+        while (i!=abjad[n].jumlah_kata)
         {
-            fwrite(&abjd[n].daftar_kata[i].panjang_kata,sizeof(int),1,fp);
-            fwrite(abjd[n].daftar_kata[i].kata,sizeof(char),abjd[n].daftar_kata[i].panjang_kata+1,fp);
-            fwrite(&abjd[n].daftar_kata[i].frekuensi,sizeof(int),1,fp);
+            fwrite(&abjad[n].daftar_kata[i].panjang_kata,sizeof(int),1,fp);
+            fwrite(abjad[n].daftar_kata[i].kata,sizeof(char),abjad[n].daftar_kata[i].panjang_kata+1,fp);
+            fwrite(&abjad[n].daftar_kata[i].frekuensi,sizeof(int),1,fp);
             i++;
         }
         n++;
@@ -123,7 +124,7 @@ void simpan_ke_biner(Abjad abjd[]){
     fclose(fp);
 }
 
-void ambil_dari_biner(Abjad abjd_baca[],int n){
+void ambil_dari_biner(int n){
     Abjad sampah[26];
     FILE *fp=fopen("binary.bin","rb");
 
@@ -140,53 +141,43 @@ void ambil_dari_biner(Abjad abjd_baca[],int n){
     }
     rewind(fp);
 
-    //baca file binary
+    //baca file binary dan tampilin ke terminal
     int i=0;
     int j;
     while (i!=26)
     {
         fread(&abjd_baca[i].abjad,sizeof(char),1,fp);
+        printf("%c\t{",abjd_baca[i].abjad);
         fread(&abjd_baca[i].jumlah_kata,sizeof(int),1,fp);
+        abjd_baca[i].daftar_kata=malloc(abjd_baca[i].jumlah_kata*sizeof(Kata));
         j=0;
         int k=0;
+        int batas;
+        if (abjd_baca[i].jumlah_kata<=n)
+        { batas=abjd_baca[i].jumlah_kata; }
+        else if (abjd_baca[i].jumlah_kata>n)
+        { batas=n; }
+        
         while (j!=abjd_baca[i].jumlah_kata)
         {
-            if (j<n)
+            if (j<batas)
             {
                 fread(&abjd_baca[i].daftar_kata[j].panjang_kata,sizeof(int),1,fp);
+                abjd_baca[i].daftar_kata[j].kata=malloc(abjd_baca[i].daftar_kata[j].panjang_kata+1);
                 fread(abjd_baca[i].daftar_kata[j].kata,sizeof(char),abjd_baca[i].daftar_kata[j].panjang_kata+1,fp);
+                printf("%s",abjd_baca[i].daftar_kata[j].kata);
                 fread(&abjd_baca[i].daftar_kata[j].frekuensi,sizeof(int),1,fp); 
+                printf("(%d)",abjd_baca[i].daftar_kata[j].frekuensi);
             }
+            if (j<batas-1)
+            { printf(","); }
             
-            else if (j>=n)
+            else if (j>=batas)
             { 
                 fread(&sampah[i].daftar_kata[k].panjang_kata,sizeof(int),1,fp);
                 fread(sampah[i].daftar_kata[k].kata,sizeof(char),sampah[i].daftar_kata[k].panjang_kata+1,fp);
                 fread(&sampah[i].daftar_kata[k].frekuensi,sizeof(int),1,fp); k++;
             }  
-            j++;
-        }
-        i++;
-    }
-    
-    //tampilin ke terminal
-    i=0;
-    while (i!=26)
-    {
-        printf("%c\t{",abjd_baca[i].abjad);
-        int batas;
-        if (abjd_baca[i].jumlah_kata <= n)
-        { batas=abjd_baca[i].jumlah_kata; }
-        else
-        { batas=n; }
-        
-        j=0;
-        while (j!=batas)
-        {
-            printf("%s",abjd_baca[i].daftar_kata[j].kata);
-            printf("(%d)",abjd_baca[i].daftar_kata[j].frekuensi);
-            if (j<batas-1)
-            { printf(","); }
             j++;
         }
         printf("}\n");
