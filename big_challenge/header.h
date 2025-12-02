b61b3f42df42e0088b72c553828b4c47904384ca
dzakybelajar <dzakysyraj@mhs.usk.ac.id>
2025-12-02 11:18:42 +0700

    tes

--- big_challenge/header.h
+++ big_challenge/header.h
@@ -12,6 +12,6 @@ typedef struct {
 
 void olah_teks(char file[]);
 void hitung_frekuensi(Kata freq);
-void pengurutan(Abjad abjad[]);
+void pengurutan();
 void simpan_ke_biner();
 void ambil_dari_biner(int);
